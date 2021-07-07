e8c9507de8e66e26f6ad00e410b608755d1d497c
Quincy Hammon <qspeed@yahoo.com>
2021-07-06 23:30:34 -0500

    PL 1st commit

---
+++ PigLatin.js
@@ -0,0 +1,90 @@
+'use strict';
+
+// brings in the assert module for unit testing
+const assert = require('assert');
+// brings in the readline module to access the command line
+const readline = require('readline');
+// use the readline module to print out to the command line
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+const pigLatin = (word) => {
+    
+        let vowels = /[aeiou]/gi;
+        let newWord = word.toLowerCase();
+        let charIndex;
+
+        // If the first letter of the word is a vowel(regEX), then add 'yay' at the end.
+        if (newWord[0].match(vowels)) {
+          return newWord + "yay";
+        }
+        // For the new word find the first vowel index.
+        for (char of newWord) {
+          if (char.match(vowels)) {
+            charIndex = newWord.indexOf(char);
+            break;
+          }
+        }
+
+        // Return the word split at the vowel and add the beginning (if !vowel) + 'ay'. 
+        return newWord.slice(charIndex) + newWord.slice(0, charIndex) + "ay";
+      }
+  // Your code here
+
+}
+
+// the first function called in the program to get an input from the user
+// to run the function use the command: node main.js
+// to close it ctrl + C
+const getPrompt = () => {
+  rl.question('word ', (answer) => {
+    console.log( pigLatin(answer) );
+    getPrompt();
+  });
+}
+
+// Unit Tests
+// to use them run the command: npm test main.js
+// to close them ctrl + C
+if (typeof describe === 'function') {
+
+  describe('#pigLatin()', () => {
+    it('should translate a simple word', () => {
+      assert.equal(pigLatin('car'), 'arcay');
+      assert.equal(pigLatin('dog'), 'ogday');
+    });
+    it('should translate a complex word', () => {
+      assert.equal(pigLatin('create'), 'eatecray');
+      assert.equal(pigLatin('valley'), 'alleyvay');
+    });
+    it('should attach "yay" if word begins with vowel', () => {
+      assert.equal(pigLatin('egg'), 'eggyay');
+      assert.equal(pigLatin('emission'), 'emissionyay');
+    });
+    it('should lowercase and trim word before translation', () => {
+      assert.equal(pigLatin('HeLlO '), 'ellohay');
+      assert.equal(pigLatin(' RoCkEt'), 'ocketray');
+    });
+  });
+} else {
+
+  getPrompt();
+
+}
+
+
+
+
+
+
+// **********
+//   HINTS
+// **********
+
+// break your code into pieces and focus on one piece at a time...
+// 1. if word begins with a vowel send to one function: adds "yay"
+// 2. if word begins with a consonant send to another function: splices off beginning, returns word with new ending.
+// 3. if multiple words, create array of words, loop over them, sending them to different functions and creating a new array with the new words.
